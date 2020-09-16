23ff02ece4c8b337cd1113f929cae5e25818bd89
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-09-15 23:23:02 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,8 @@
 //  * Defeat each enemy robot
 //"LOSE" - Player robot's health is zero or less
 
-//if the enemy robot's health is zero ort less, exit from the fight loop
+//if the enemy robot's health is zero or less, exit from the fight loop
+
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -76,6 +77,14 @@ var fight = function(enemyName) {
   }
 }
 
+//function to start a new game
+var startGame = function (){
+  //reset player status
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+
 for(var i = 0; i < enemyNames.length; i++) {
   if(playerHealth > 0) {
     window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
@@ -97,3 +106,29 @@ for(var i = 0; i < enemyNames.length; i++) {
   }
 }
 
+//play again
+endGame();
+};
+
+//function to end the entire game
+var endGame = function() {
+  //if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've servived the game! You now have a score of " + playerMoney + ".");
+  }
+  else{
+  window.alert("the game has now ended. Let's see how you did!");
+  }
+
+  var playAgainConfirm = window.confirm('Would you like to play again?');
+
+  if (playAgainConfirm) {
+    //restart the game
+    startGame();
+  }
+  else{
+    window.alert("Thank you for playing Robot Galdiators! Come back soon!");
+  }
+
+};
+ startGame();
